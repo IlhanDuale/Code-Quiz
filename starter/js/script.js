89c1471d93e4d750df87918552166983cacc2e35
Ilhan <dualeilhan@gmail.com>
2023-11-13 11:55:57 +0000

    an event listener for the DOMContentLoaded event

--- starter/js/script.js
+++ starter/js/script.js
@@ -1,64 +1,96 @@
-var startButton = document.querySelector("#start");
-var startScreen = document.querySelector("#start-screen");
-var questionsEl = document.querySelector('#questions');
-var questionTitle = document.querySelector('#question-title');
-var choices = document.querySelector('#choices');
-var questionCount = 0;
-var feedback = document.querySelector("#feedback");
-var initialsInput = document.querySelector("#initials");
-var submitScoreButton = document.querySelector("#submit-score");
-var scoreScreen = document.querySelector("#score-screen");
-var finalScore = document.querySelector("#final-score");
+document.addEventListener('DOMContentLoaded', function() {
+    var startButton = document.querySelector("#start");
+    var startScreen = document.querySelector("#start-screen");
+    var questionsEl = document.querySelector('#questions');
+    var questionTitle = document.querySelector('#question-title');
+    var choices = document.querySelector('#choices');
+    var timerEl = document.querySelector('#time'); // Update to match the correct ID
+    var feedback = document.querySelector("#feedback");
+    var initialsInput = document.querySelector("#initials");
+    var submitScoreButton = document.querySelector("#submit");
+    var endScreen = document.querySelector("#end-screen");
+    var finalScore = document.querySelector("#final-score");
 
-var questionCount = 0;
-var time = 60; // time limit
-var timerInterval;
+    var questionCount = 0;
+    var time = 60; // time limit
+    var timerInterval;
+    var score = 0;
 
-startButton.addEventListener("click", function() {
-    startScreen.classList.add("hide")
-    startQuiz(); //displayQuestion()
-})
+    startButton.addEventListener("click", function() {
+        startScreen.classList.add("hide");
+        startQuiz();
+    });
 
-function startQuiz() {
-    timerInterval = setInterval(function() {
-        time--;
-        timerEl.textContent = 'Time: ' + time + ' seconds';
+    function startQuiz() {
+        timerInterval = setInterval(function() {
+            time--;
+            timerEl.textContent = 'Time: ' + time + ' seconds';
 
-        if (time <= 0 || questionCount >= questions.length) {
+            if (time <= 0 || questionCount >= questions.length) {
+                endQuiz();
+            }
+        }, 1000);
+
+        displayQuestion();
+    }
+
+    function displayQuestion() {
+        questionTitle.innerHTML = '';
+        choices.innerHTML = '';
+        feedback.textContent = '';
+        questionTitle.textContent = questions[questionCount].title;
+
+        questions[questionCount].choices.forEach(function(choice) {
+            var button = document.createElement('button');
+            button.textContent = choice;
+            button.value = choice;
+            button.addEventListener('click', checkAnswer);
+            choices.append(button);
+        });
+
+        questionsEl.classList.remove('hide');
+    }
+
+    function checkAnswer(event) {
+        if (event.target.value === questions[questionCount].answer) {
+            feedback.textContent = 'Correct!';
+            score += 10;
+        } else {
+            feedback.textContent = 'Incorrect!';
+            time -= 10;
+        }
+
+        questionCount++;
+
+        if (questionCount < questions.length) {
+            displayQuestion();
+        } else {
             endQuiz();
         }
-    }, 1000);
-
-    displayQuestion();
-}
-
-function displayQuestion() {
-    questionTitle.innerHTML = '';
-    choices.innerHTML = '';
-    feedback.textContent = '';
-    questionTitle.textContent = questions[questionCount].title;
-
-    questionTitle.textContent = questions[questionCount].title
-    questions[questionCount].choices.forEach(function(choice) {
-        var button = document.createElement('button')
-        button.textContent = choice
-        button.value = choice
-        button.addEventListener('click', checkAnswer);
-        choices.append(button);
-    })
-    questionsEl.classList.remove('hide');
-}
-
-function checkAnswer(event) {
-    if (event.target.value === questions[questionCount].answer) {
-        feedback.textContent = 'correct';
-    } else {
-        feedback.textContent = 'incorrect';
     }
-    if (questionCount + 1 < questions.length) {
-        questionCount++
-        displayQuestion()
-    } else {
-        // display score page here
+
+    function endQuiz() {
+        clearInterval(timerInterval);
+        questionsEl.classList.add('hide');
+        endScreen.classList.remove('hide');
+        finalScore.textContent = score;
     }
-}
+
+    submitScoreButton.addEventListener('click', function() {
+        var initials = initialsInput.value.trim();
+
+        if (initials !== '') {
+            var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+            var newScore = { initials: initials, score: score };
+            highScores.push(newScore);
+
+            highScores.sort(function(a, b) {
+                return b.score - a.score;
+            });
+
+            localStorage.setItem('highScores', JSON.stringify(highScores));
+
+            window.location.href = 'highscores.html';
+        }
+    });
+});
