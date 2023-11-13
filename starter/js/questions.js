5aa5cb8bc2a554b18ec8d061a26400610629aa90
Ilhan <dualeilhan@gmail.com>
2023-11-13 12:05:57 +0000

    questions.js has been updated with more questions and answers

--- starter/js/questions.js
+++ starter/js/questions.js
@@ -1,9 +1,19 @@
 var questions = [{
-    title: 'How are you?',
-    choices: ['good', 'fine', 'not well'],
-    answer: 'good',
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['Strings', 'Boolean', 'alerts', 'Numbers'],
+    answer: 'alerts',
 }, {
-    title: 'ferhfge',
-    choices: ['ggfr', 'Hi', 'ok'],
-    answer: 'ok',
+    title: 'The condition in an if/else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'paranthesis', 'square brackets'],
+    answer: 'curly brackets',
+}, {
+    title: 'Arrays in Javascript can be used to ____.',
+    choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+    answer: 'all of the above',
+}, {
+
+}, {
+    title: 'String values must be enclosed within ____ when being assigned to variables',
+    choices: ['commas', 'curly brackets', 'quotes', 'paranthesis'],
+    answer: 'quotes',
 }]
